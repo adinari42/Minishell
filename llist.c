1a1beea36d8ed9969a2efe0fb0bb4c2923e300a6
Stephanie Lakner <stephanie.lakner@rub.de>
2022-11-14 00:16:32 +0100

    added some files that I forgot

---
+++ llist.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   llist.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 21:49:16 by slakner           #+#    #+#             */
+/*   Updated: 2022/11/13 23:49:27 by slakner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_token	*token_new(char *str)
+{
+	t_token		*newtoken;
+	static int	idx;
+
+	newtoken = malloc(sizeof(t_token));
+	if (newtoken)
+	{
+		newtoken->str = str;
+		newtoken->type = token_type(str);
+		newtoken->id = idx;
+		newtoken->prev = NULL;
+		newtoken->next = NULL;
+	}
+	idx ++;
+	return (newtoken);
+}
+
+void	append(t_token **token, t_token *new_elem)
+{
+	t_token	*top;
+
+	if (!token || !new_elem)
+		return ;
+	if (!*token)
+	{
+		*token = new_elem;
+		return ;
+	}
+	top = list_end(token);
+	if (top)
+	{
+		top->next = new_elem;
+		new_elem->prev = top;
+	}
+}
+
+t_token	*list_end(t_token **token)
+{
+	t_token	*elemptr;
+
+	elemptr = NULL;
+	if (token && *token)
+	{
+		elemptr = *token;
+		while (elemptr && elemptr->next)
+		{
+			elemptr = elemptr->next;
+		}
+		return (elemptr);
+	}
+	return (NULL);
+}
+
+t_token	*list_start(t_token **token)
+{
+	if (token && *token)
+		return (*token);
+	return (NULL);
+}
+
+// t_token	*pop(t_token **token)
+// {
+// 	t_token	*old_top;
+
+// 	old_top = NULL;
+// 	if (token && *token)
+// 	{
+// 		old_top = token_top(token);
+// 		if (old_top)
+// 		{
+// 			if (old_top->prev)
+// 			{
+// 				old_top->prev->next = NULL;
+// 				old_top->next = NULL;
+// 				old_top->prev = NULL;
+// 			}
+// 		}
+// 	}
+// 	if (*token == old_top)
+// 		*token = NULL;
+// 	return (old_top);
+// }
---
+++ tokens.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokens.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 20:39:48 by slakner           #+#    #+#             */
+/*   Updated: 2022/11/14 00:09:10 by slakner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	char_in_charset(char c, const char *charset)
+{
+	char	*ptr;
+
+	ptr = (char *) charset;
+	while (*ptr)
+	{
+		if (c == *ptr)
+			return (1);
+		ptr ++;
+	}
+	return (0);
+}
+
+int	token_type(char *c)
+{
+	if (!ft_strncmp(c, "<<", 2))
+		return (APPEND_IN);
+	else if (!ft_strncmp(c, ">>", 2))
+		return (APPEND_OUT);
+	else if (!ft_strncmp(c, "<", 1))
+		return (REDIR_IN);
+	else if (!ft_strncmp(c, ">", 1))
+		return (REDIR_OUT);
+	else if (!ft_strncmp(c, "\"", 1))
+		return (DOUBLE_QUOTE);
+	else if (!ft_strncmp(c, "'", 1))
+		return (SINGLE_QUOTE);
+	else if (!ft_strncmp(c, "|", 1))
+		return (PIPE);
+	else if (!ft_strncmp(c, " ", 1))
+		return(SPACE);
+	else
+		return (WORD);
+}
+
+void print_list(t_token *tklist)
+{
+	if (!tklist)
+		return;
+
+	int i = 0;
+	while (tklist)
+	{
+		printf(">%s\n", tklist->str);
+		tklist = tklist->next;
+		i++;
+	}
+	return ;
+}
+
+
+t_token	**read_tokens(char *bashcmd)
+{
+	const char	spec_c[] = "\"'<>| ";
+	t_token		*token;
+	t_token		**tk_list;
+	size_t		word_s;
+	size_t		i;
+	char		*tokenstr;
+
+	tk_list = malloc(sizeof(t_list *));
+	*tk_list = NULL;
+	word_s = 0;
+	i = 0;
+	while (i < ft_strlen(bashcmd))
+	{
+		if (char_in_charset(bashcmd[i], spec_c) || bashcmd[i] == ' ')
+		{
+			if (i - word_s > 1) // we need to save the previous word
+			{
+				tokenstr = ft_substr(bashcmd, word_s, i - word_s); //TODO: free this
+				token = token_new(tokenstr);
+				append(tk_list, token);
+			}
+			//now save the char that we just found
+			if (bashcmd[i] == '<' || bashcmd[i] == '>')
+			{
+				if (i < ft_strlen(bashcmd) && bashcmd[i + 1] == '<')
+				{
+					token = token_new(ft_strdup("<<"));
+					i++;
+				}
+				else if (i < ft_strlen(bashcmd) && bashcmd[i + 1] == '>')
+				{
+					token = token_new(ft_strdup(">>"));
+					i++;
+				}
+			}
+			else
+				token = token_new(ft_substr(bashcmd, i, 1));
+			word_s = i + 1;
+			append(tk_list, token);
+		}
+		i++;
+	}
+	if (word_s < i)
+	{
+		token = token_new(ft_substr(bashcmd, word_s, i - word_s));
+		append(tk_list, token);
+		print_list(*tk_list);
+	}
+
+
+	return (tk_list);
+}
