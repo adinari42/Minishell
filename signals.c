69b6c68bdfd73ef28af9eb78ee5f4f2feb4b5f88
Stephanie Lakner <stephanie.lakner@rub.de>
2022-11-14 00:16:32 +0100

    added some files that I forgot

--- signals.c
+++ signals.c
@@ -6,102 +6,11 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 20:01:42 by slakner           #+#    #+#             */
-/*   Updated: 2022/12/04 20:10:05 by slakner          ###   ########.fr       */
+/*   Updated: 2022/11/13 22:45:51 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-void	nul(int signum)
-{
-	//char *pos;
-
-	if (signum == SIGQUIT)
-	{
-		rl_redisplay();
-	}
-}
-
-void heredoc_sigint_handler(int signum)
-{
-	if (signum == SIGINT)
-	{
-		g_stop = 1;
-	}
-}
-
-void heredoc_sigquit_handler(int signum)
-{
-	if (signum == SIGQUIT)
-	{
-		g_stop = 1;
-	}
-}
-
-void	heredoc_signals(int fd)
-{
-	struct termios	t_settings;
-
-	signal(SIGINT, heredoc_sigint_handler);
-	signal(SIGQUIT, nul);
-	tcgetattr(fd, &t_settings);
-    t_settings.c_lflag &= ~ICANON;
-	t_settings.c_lflag |=~ECHOCTL;
-	t_settings.c_oflag &= ~OCRNL;
-    t_settings.c_cc[VMIN] = 1;
-    t_settings.c_cc[VTIME] = 0;
-    tcsetattr(fd, TCSANOW, &t_settings);
-}
-
-void	minishell_new_prompt(int signum)
-{
-	if (signum == SIGINT)
-	{
-		write (1, "\n", 1);
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		rl_redisplay();
-	}
-}
-
-void	minishell_new_prompt_blocking(int signum)
-{
-	if (signum == SIGINT)
-	{
-		write (1, "\n", 1);
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		// rl_redisplay();
-	}
-}
-
-void	sigquit_blocking(int signum)
-{
-	if (signum == SIGQUIT)
-	{
-		printf("Quit: 3\n");
-		rl_redisplay();
-	}
-}
-
-void	signals_blocking_command(void)
-{
-	struct termios	t_settings;
-
-	signal(SIGINT, minishell_new_prompt_blocking);
-	signal(SIGQUIT, sigquit_blocking);
-	tcgetattr(1, &t_settings);
-	// printf("ECHOCTL: %d %x \n", ECHOCTL, ECHOCTL);
-	// printf("tcgetattr.c_lflag: %lu\n", t_settings.c_lflag);
-	t_settings.c_lflag |=ECHOCTL;
-	// printf("tcgetattr.c_lflag: %lu\n", t_settings.c_lflag);
-	tcsetattr(1, 0, &t_settings);
-}
-
-// Ctrl-C: SIGINT
-// Ctrl-\: SIGQUIT
 void	init_signals(void)
 {
-	signal(SIGINT, minishell_new_prompt);
-	signal(SIGQUIT, nul);
+
 }
--- tokens.c
+++ tokens.c
@@ -6,7 +6,7 @@
 /*   By: adinari <adinari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 20:39:48 by slakner           #+#    #+#             */
-/*   Updated: 2022/12/23 23:30:55 by adinari          ###   ########.fr       */
+/*   Updated: 2022/11/14 00:09:10 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,76 +43,78 @@ int	token_type(char *c)
 	else if (!ft_strncmp(c, "|", 1))
 		return (PIPE);
 	else if (!ft_strncmp(c, " ", 1))
-		return (SPACE_TKN);
-	else if (!ft_strncmp(c, "=", 1))
-		return (ASSIGN);
+		return(SPACE);
 	else
 		return (WORD);
 }
 
-void	print_list(t_token *list)
+void print_list(t_token *tklist)
 {
-	t_token	*tklist;
-
-	tklist = list;
 	if (!tklist)
-		return ;
+		return;
+
+	int i = 0;
 	while (tklist)
 	{
-		printf(">%s %d\n", tklist->str, tklist->type);
+		printf(">%s\n", tklist->str);
 		tklist = tklist->next;
+		i++;
 	}
 	return ;
 }
 
-void append_from_str(t_token **list, char *str)
-{
-	tappend(list, token_new(str));
-	free(str);
-}
-
-
-char	*non_word_tknstr(char *bashcmd, size_t i)
-{
-	char	*tokenstr;
-
-	if (i < ft_strlen(bashcmd) - 1
-		&& bashcmd[i] == '<' && bashcmd[i + 1] == '<' && ++i)
-		tokenstr = ft_strdup("<<");
-	else if (i < ft_strlen(bashcmd) - 1
-		&& bashcmd[i] == '>' && bashcmd[i + 1] == '>' && ++i)
-		tokenstr = ft_strdup(">>");
-	else
-		tokenstr = ft_substr(bashcmd, i, 1);
-	return (tokenstr);
-}
 
 t_token	**read_tokens(char *bashcmd)
 {
-	const char	spec_c[] = "\"'<>| =";
+	const char	spec_c[] = "\"'<>| ";
+	t_token		*token;
 	t_token		**tk_list;
 	size_t		word_s;
 	size_t		i;
+	char		*tokenstr;
 
-	tk_list = malloc(sizeof(t_token *));
+	tk_list = malloc(sizeof(t_list *));
 	*tk_list = NULL;
 	word_s = 0;
 	i = 0;
-	while (i < ft_strlen(bashcmd) && bashcmd[i] == ' ')
-		i++;
-	word_s = i;
 	while (i < ft_strlen(bashcmd))
 	{
-		if (char_in_charset(bashcmd[i], spec_c))
+		if (char_in_charset(bashcmd[i], spec_c) || bashcmd[i] == ' ')
 		{
-			if (i - word_s > 0)
-				append_from_str(tk_list, ft_substr(bashcmd, word_s, i - word_s));
+			if (i - word_s > 1) // we need to save the previous word
+			{
+				tokenstr = ft_substr(bashcmd, word_s, i - word_s); //TODO: free this
+				token = token_new(tokenstr);
+				append(tk_list, token);
+			}
+			//now save the char that we just found
+			if (bashcmd[i] == '<' || bashcmd[i] == '>')
+			{
+				if (i < ft_strlen(bashcmd) && bashcmd[i + 1] == '<')
+				{
+					token = token_new(ft_strdup("<<"));
+					i++;
+				}
+				else if (i < ft_strlen(bashcmd) && bashcmd[i + 1] == '>')
+				{
+					token = token_new(ft_strdup(">>"));
+					i++;
+				}
+			}
+			else
+				token = token_new(ft_substr(bashcmd, i, 1));
 			word_s = i + 1;
-			append_from_str(tk_list, non_word_tknstr(bashcmd, i));
+			append(tk_list, token);
 		}
 		i++;
 	}
 	if (word_s < i)
-		append_from_str(tk_list, ft_substr(bashcmd, word_s, i - word_s));
+	{
+		token = token_new(ft_substr(bashcmd, word_s, i - word_s));
+		append(tk_list, token);
+		print_list(*tk_list);
+	}
+
+
 	return (tk_list);
 }
