6fbafd8d761ac2b08767905d4bc65000bee267ca
Stephanie Lakner <stephanie.lakner@rub.de>
2022-11-18 22:32:31 +0100

    Fixed some segfaults in export function, but there is more to be done

--- builtins.c
+++ builtins.c
@@ -6,7 +6,7 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 20:03:18 by slakner           #+#    #+#             */
-/*   Updated: 2022/11/18 19:56:32 by slakner          ###   ########.fr       */
+/*   Updated: 2022/11/18 22:28:23 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,10 @@ int	exec_echo(t_token **list)
 		return (1);
 	}
 	if (token->next && !ft_strncmp(token->next->str, "-n", 3))
+	{
+		token = token->next;
 		newline = 0;
+	}
 	while (token->next)
 	{
 		token = token->next;
@@ -87,102 +90,9 @@ int	exec_cd(t_token **list)
 	ret = chdir(list_start(list)->next->str);
 	if (ret == -1)
 		printf("cd: no such file or directory: %s\n", list_start(list)->next->str);
-	//printf("%d \n", ret);
 	return (ret);
 }
 
-char	**envp_parse(char **envp)
-{
-	int		j;
-	char	**envp_parse;
-
-	j = 0;
-	while (envp[j])
-	{
-		printf("envp[i]: %s\n", envp[j]);
-		j++;
-	}
-	j = -1;
-	while (envp[++j])
-	{
-		if (!ft_strncmp(envp[j], "PATH=", 5))
-			break ;
-	}
-	envp_parse = ft_split(*(envp + j) + 5, ':');
-	return (envp_parse);
-}
-
-
-//assumption here: quotes have been stripped already
-char	*extract_varname_quoted(char *tokenstr)
-{
-	char	*varname;
-	char	*ptr;
-	char	**split;
-
-	ptr = ft_strchr(tokenstr, '=');
-	if (!ptr)	// case: no equal sign found in string, probably needs actual error management to behave like bash
-		return (NULL);
-	else
-	{
-		split = ft_split(tokenstr, '=');
-		varname = ft_strdup(split[0]);
-		free_split(split);
-	}
-	if (ft_strchr(varname, ' '))
-	{
-		printf("export: not valid in this context: %s\n", varname);
-		free(varname);
-	}
-	printf("varname from extract_varname_quoted: %s\n", varname);
-	return (varname);
-}
-
-char *extract_value(char *tokenstr)
-{
-	char	*varname;
-	char	*ptr;
-	char	**split;
-
-	ptr = ft_strchr(tokenstr, '=');
-	if (!ptr)	// case: no equal sign found in string, probably needs actual error management to behave like bash
-		return (NULL);
-	else
-	{
-		split = ft_split(tokenstr, '=');
-		varname = ft_strdup(split[1]);
-		free_split(split);
-	}
-	printf("varname from extract_varname_quoted: %s\n", varname);
-	return (varname);
-}
-
-int	var_in_env(char *varname)
-{
-	int		i;
-	char	**split;
-
-	i = 0;
-	while (g_envp[i])
-	{
-		split = ft_split(g_envp[i], '=');
-		if (!ft_strncmp(split[0], varname, ft_strlen(split[0]) + 1))
-		{
-			free_split(split);
-			return (i);
-		}
-		free_split(split);
-		i++;
-	}
-	return (-1);
-}
-
-void	display_env(void)
-{
-	return ;
-}
-
-// these functions are emtpy dummy functions for now so it compiles
 int	exec_export(t_token **list)
 {
 	int		ret;
@@ -190,12 +100,15 @@ int	exec_export(t_token **list)
 	char	*varname;
 	char	*value;
 	int		arraypos;
-	
+	char	*newstr;
+
+	varname = NULL;
+
 	token = list_start(list);
 	ret = 0;
 	if (ft_strncmp(token->str, "export", 7))
 	{
-		printf("Something went wrong here, %s is not the cd command\n",
+		printf("Something went wrong here, %s is not the export command\n",
 			token->str);
 		return (1);
 	}
@@ -207,9 +120,9 @@ int	exec_export(t_token **list)
 		return (0);
 	}
 	token = token->next;
-	if (token->type == WORD)
+	if (token->type == WORD && ft_strlen(token->str))
 	{
-		varname = ft_strdup(token->next->str);
+		varname = ft_strdup(token->str);
 		value = NULL;
 		if (token->next && token->next->type == EQUAL)
 		{
@@ -218,23 +131,33 @@ int	exec_export(t_token **list)
 				value = ft_strdup(token->next->str);
 		}
 	}
-	else if (token->next->type == STR_DQUOTES || token->next->type == STR_SQUOTES)
+	else if (token->type == STR_DQUOTES || token->type == STR_SQUOTES)
 	{
 		varname = extract_varname_quoted(token->next->str);
 		value = extract_value(token->next->str);
 	}
 	else
+	{
 		varname = NULL; // TODO: needs actual error management if the token after 'export' and some spaces is not WORD or STR_...
+		display_env();
+		return (0);
+	}
 	arraypos = var_in_env(varname);
+	free(g_envp[arraypos]);
+	newstr = ft_strjoin(varname, "=");
+	g_envp[arraypos] = ft_strjoin(newstr, value);
+	
 	// if (arraypos != -1)
 	// 	; // TODO: update variables
-	// else
-	// 	;// TODO: handle nonexistent variable names here
-	ret = 0;
+	// else if (token->prev->type = EQUALS)
+	// 	;// TODO: add variable names here, if there is an EQUAL sign
+	// do nothing if there is no assignment operator
+	free(newstr);
 	free(varname);
-	return (ret);
+	return (0);
 }
 
+// these functions are emtpy dummy functions for now so it compiles
 int	exec_unset(t_token **list)
 {
 	int	ret;
@@ -246,13 +169,27 @@ int	exec_unset(t_token **list)
 
 int	exec_env(t_token **list)
 {
-	int	ret;
+	t_token	*token;
 
-	(void) list;
-	ret = 0;
-	return (ret);
+	token = list_start(list);
+	if (ft_strncmp(token->str, "env", 7))
+	{
+		printf("Something went wrong here, %s is not the cd command\n",
+			token->str);
+		return (1);
+	}
+	while (token->next)
+	{
+		if (token->next->type != SPACE)
+		{
+			printf("env: %s: No such file or directory\n", token->next->str);
+			return (1);
+		}
+	}
+	display_env();
+	return (0);
 }
-
+ 
 int	exec_exit(t_token **list)
 {
 	int	ret;
--- env.c
+++ env.c
@@ -6,13 +6,101 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 22:30:12 by slakner           #+#    #+#             */
-/*   Updated: 2022/11/14 22:31:29 by slakner          ###   ########.fr       */
+/*   Updated: 2022/11/18 22:12:05 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
-
-int env_init(char **env)
+char	**envp_parse(char **envp)
 {
-	
+	int		j;
+	char	**envp_parse;
+
+	j = -1;
+	while (envp[++j])
+	{
+		if (!ft_strncmp(envp[j], "PATH=", 5))
+			break ;
+	}
+	envp_parse = ft_split(*(envp + j) + 5, ':');
+	return (envp_parse);
+}
+
+
+//assumption here: quotes have been stripped already
+char	*extract_varname_quoted(char *tokenstr)
+{
+	char	*varname;
+	char	*ptr;
+	char	**split;
+
+	ptr = ft_strchr(tokenstr, '=');
+	if (!ptr)	// case: no equal sign found in string, probably needs actual error management to behave like bash
+		return (NULL);
+	else
+	{
+		split = ft_split(tokenstr, '=');
+		varname = ft_strdup(split[0]);
+		free_split(split);
+	}
+	if (ft_strchr(varname, ' '))
+	{
+		printf("export: not valid in this context: %s\n", varname);
+		free(varname);
+	}
+	printf("varname from extract_varname_quoted: %s\n", varname);
+	return (varname);
+}
+
+char	*extract_value(char *tokenstr)
+{
+	char	*varname;
+	char	*ptr;
+	char	**split;
+
+	ptr = ft_strchr(tokenstr, '=');
+	if (!ptr)	// case: no equal sign found in string, probably needs actual error management to behave like bash
+		return (NULL);
+	else
+	{
+		split = ft_split(tokenstr, '=');
+		varname = ft_strdup(split[1]);
+		free_split(split);
+	}
+	printf("varname from extract_varname_quoted: %s\n", varname);
+	return (varname);
+}
+
+int	var_in_env(char *varname)
+{
+	int		i;
+	char	**split;
+
+	i = 0;
+	while (g_envp[i] && varname)
+	{
+		split = ft_split(g_envp[i], '=');
+		if (!ft_strncmp(split[0], varname, ft_strlen(split[0]) + 1))
+		{
+			free_split(split);
+			return (i);
+		}
+		free_split(split);
+		i++;
+	}
+	return (-1);
+}
+
+void	display_env(void)
+{
+	int		i;
+
+	i = 0;
+	while (g_envp[i])
+	{
+		printf("%s\n",g_envp[i]);
+		i++;
+	}
+	return ;
 }
