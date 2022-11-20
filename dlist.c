7a3b3db02058bbd64d88b9c79770395de95354b5
Stephanie Lakner <stephanie.lakner@rub.de>
2022-11-20 23:22:06 +0100

    Files for linked lists were missing

--- dlist.c
+++ dlist.c
@@ -6,7 +6,7 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 18:15:44 by slakner           #+#    #+#             */
-/*   Updated: 2022/12/09 19:24:28 by slakner          ###   ########.fr       */
+/*   Updated: 2022/11/20 20:29:27 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	lstsize(t_dlist *lst)
 
 	if (!lst)
 		return (0);
-	cnt = 0;
-	while (lst)
+	cnt = 1;
+	while (lst->next)
 	{
 		cnt ++;
 		lst = lst->next;
@@ -31,7 +31,7 @@ void	lstadd_back(t_dlist **lst, t_dlist *new)
 {
 	t_dlist	*last;
 
-	if (!new && !lst)
+	if (!lst || !new)
 		return ;
 	if (!*lst)
 	{
@@ -52,6 +52,7 @@ void	lstadd_front(t_dlist **lst, t_dlist *new)
 	}	
 }
 
+
 t_dlist	*lstlast(t_dlist *lst)
 {
 	if (!lst)
@@ -61,7 +62,7 @@ t_dlist	*lstlast(t_dlist *lst)
 	return (lst);
 }
 
-t_dlist	*lstnew(t_kval *content)
+t_dlist	*lstnew(void *content)
 {
 	t_dlist	*newlist;
 
@@ -73,29 +74,3 @@ t_dlist	*lstnew(t_kval *content)
 	return (newlist);
 }
 
-void	lstdel_elem(t_dlist **lst, t_dlist *elem)
-{
-	t_dlist	*cur;
-
-	cur = *lst;
-	if (cur == elem)
-	{
-		*lst = cur->next;
-		if (cur->content)
-			free_kval(cur->content);
-		free(cur);
-		return ;
-	}
-	while (cur)
-	{
-		if (cur->next && cur->next == elem)
-		{	
-			cur->next = elem->next;
-			if (elem->content)
-				free_kval(elem->content);
-			free(elem);
-			break ;
-		}
-		cur = cur->next;
-	}
-}
