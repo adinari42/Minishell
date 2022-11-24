b35836063cd1675380f77fbed96d349f55aebc15
Stephanie Lakner <stephanie.lakner@rub.de>
2022-11-24 18:20:31 +0100

    forgot to add the includes

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,23 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stephanie.lakner <stephanie.lakner@stud    +#+  +:+       +#+        */
+/*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 22:49:44 by adinari           #+#    #+#             */
-/*   Updated: 2022/12/26 13:36:37 by stephanie.l      ###   ########.fr       */
+/*   Updated: 2022/11/24 18:13:10 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <sys/types.h>
-#include <sys/wait.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <signal.h>
 # include <unistd.h>
-# include <errno.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "structs.h"
@@ -29,16 +26,12 @@
 # include "llist.h"
 # include "env.h"
 # include "init.h"
-# include "exec.h"
-# include "string_utils.h"
-# include <termios.h>
 # include "../gnl/get_next_line.h"
-# include "pipes.h"
-# include <fcntl.h>
 
 # define TOKENS " $'<>\""
 
-int	g_stop;
+//char	**g_envp;
+t_dlist	**g_env;
 
 enum e_tokentype
 {
@@ -50,17 +43,15 @@ enum e_tokentype
 	DOUBLE_QUOTE,
 	SINGLE_QUOTE,
 	ASSIGN,
-	SPACE_TKN,
+	SPACE,
 	WORD,
 	STR_DQUOTES,
-	STR_SQUOTES,
-	INFILE,
-	OUTFILE
+	STR_SQUOTES
 };
 
 enum e_builtins
 {
-	ECHO42,
+	ECHO,
 	CD,
 	PWD,
 	EXPORT,
@@ -69,7 +60,7 @@ enum e_builtins
 	EXIT
 };
 
-static const char *const	g_builtins[] = {
+const static char *const	g_builtins[] = {
 	"echo",
 	"cd",
 	"pwd",
@@ -81,34 +72,29 @@ static const char *const	g_builtins[] = {
 
 /*tokens.c*/
 t_token	**read_tokens(char *bashcmd);
-void	set_cmd_path(t_token *tklist, t_parse parse);
 int		token_type(char *c);
-void	print_list(t_token *tklist);
+
 void	init_signals(void);
 
-t_token	*remove_spaces(t_token *list);
+t_token	**remove_spaces(t_token **list);
 t_token	**remove_empty(t_token **list);
 
 int		exec(char *program, char **args, char *const *envp);
 
 /*command.c*/
-//int		handle_commandstr(t_token **list);
-int		handle_builtin(t_token *list, t_dlist **env, t_pipe *data);
-int		handle_builtinstr(t_token *list, t_pipe *data, int i, t_dlist **env, int builtin_id);
-int	handle_command(t_pipe *data, t_token **cmd_line, int i, t_dlist **env);
-
+int		handle_commandstr(t_token **list);
+int		handle_builtin(t_token **list);
+int		handle_command(t_token **list);
 
 /*builtin.c*/
 int		is_builtin(char *str);
-int		exec_echo(t_token *token, t_dlist *env);
-int		exec_cd(t_token *token, t_dlist *env);
-int		exec_pwd(t_token *token, t_dlist *env);
-int		exec_export(t_token *token, t_dlist *env);
-int		exec_unset(t_token *token, t_dlist *env);
-int		exec_env(t_token *token, t_dlist *env);
-int		exec_exit(t_token *token, t_dlist **env);
-int		builtin_plausible(t_token *token, char *builtin);
-int		print_builtin_error(char *builtin, char *dir);
+int		exec_echo(t_token **token);
+int		exec_cd(t_token **token);
+int		exec_pwd(t_token **token);
+int		exec_export(t_token **token);
+int		exec_unset(t_token **token);
+int		exec_env(t_token **token);
+int		exec_exit(t_token **token);
 
 /*quotes.c*/
 t_token	**merge_quoted_strings(t_token **list, t_pipe *data);
@@ -116,39 +102,38 @@ t_token	*merge_tokens(t_token *first, t_token *last);
 t_token	*merge_two_tokens(t_token *token1, t_token *token2);
 
 /*exit.c*/
-void	free_and_exit(int signum, t_dlist **env);
-void	exit_with_value(int retval, t_dlist **env);
+void	free_globals(void);
+void	free_and_exit(int signum);
+
 
 /*execute_line.c*/
 void	execute_line(t_token *list, t_parse parse, char **envp);
-void	init_path(t_token **cmdline, t_parse *parse, t_dlist **env, t_pipe *data);
+void	init_path(t_token *tklist, t_parse parse);
 char	*get_path(char **string, char *cmd);
-void	ms_fd_err(int i);
-void	ms_fd_error(int i, t_pipe *data);
-/*******/
+
+t_token	**read_tokens(char *bashcmd);
+void	set_cmd_path(t_token *tklist, t_parse parse);
+int		token_type(char *c);
+void	init_signals(void);
+void	print_list(t_token *tklist);
 t_token	*token_new(char *str);
+void	free_token_list(t_token **list);
+void	free_token(t_token *elem);
 void	delete(t_token *del_elem);
 void	append(t_token **token, t_token *new_elem);
 t_token	*list_end(t_token **token);
 t_token	*list_start(t_token **token);
-
-/* spaces.c */
-t_token	*skip_spaces(t_token *token);
-
-/* current main.c */
-char	*get_cmd(t_token *list, t_pipe *data);
-void	exec_cmd(t_pipe *pipe, t_dlist **env);
-void	parent(t_pipe *pipe);
-void	free_and_close(t_pipe *pipe);
-int		init_outfile(t_pipe *pipe);
-char** set_parse_cmd(t_token *head);
-
-/*signals.c*/
-void	signals_blocking_command(void);
-void	heredoc_signals(int fd);
-
-
-
-void	print_double_ptr(char **ptr);
+/*expandvalue.c*/
+char	*value_expand(char **envp, char *var);
+char	*expand_value(char *str, char **envp);
+void	check_value(t_token *list, char **envp);
+void	free_2d(char ***to_free);
+void	free_strings(char *str, char **split1);
+char	*add_space(char *tmp, char *res);
+char	*join_to_res(char *tmp, char **split2, char *res, int j, char **envp);
+/*quotes.c*/
+// t_token	**merge_quoted_strings(t_token **list);
+// t_token	*merge_tokens(t_token *first, t_token *last);
+// t_token	*merge_two_tokens(t_token *token1, t_token *token2);
 
 #endif
