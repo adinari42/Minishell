ca3d668a6650b75261cd17562de10bce9527a07d
Stephanie Lakner <stephanie.lakner@rub.de>
2022-11-24 18:20:31 +0100

    forgot to add the includes

--- includes/llist.h
+++ includes/llist.h
@@ -6,7 +6,7 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 20:33:27 by slakner           #+#    #+#             */
-/*   Updated: 2022/12/13 21:33:38 by slakner          ###   ########.fr       */
+/*   Updated: 2022/11/21 23:17:09 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 
 t_token	*token_new(char *str);
 void	delete(t_token *del_elem);
-void	tappend(t_token **list, t_token *new_elem);
-t_token	*tlist_start(t_token *token);
-t_token	*tlist_end(t_token *token);
+void	tappend(t_token **token, t_token *new_elem);
+t_token	*tlist_start(t_token **token);
+t_token	*tlist_end(t_token **token);
 void	print_list(t_token *tklist);
 
 int		lstsize(t_dlist *lst);
@@ -26,6 +26,5 @@ void	lstadd_back(t_dlist **lst, t_dlist *new);
 void	lstadd_front(t_dlist **lst, t_dlist *new);
 t_dlist	*lstlast(t_dlist *lst);
 t_dlist	*lstnew(t_kval *content);
-void	lstdel_elem(t_dlist **lst, t_dlist *elem);
 
 #endif
