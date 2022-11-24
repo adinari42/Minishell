fcbdba369291068be8598db95816674be9528e7b
Stephanie Lakner <stephanie.lakner@rub.de>
2022-11-24 19:07:03 +0100

    update to ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: adinari <adinari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/03 02:30:21 by adinari           #+#    #+#             */
-/*   Updated: 2022/10/18 23:08:50 by adinari          ###   ########.fr       */
+/*   Created: 2022/03/24 19:11:23 by slakner           #+#    #+#             */
+/*   Updated: 2022/11/24 19:06:12 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*join_spot;
 	size_t	len;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
-	join_spot = malloc(ft_strlen(s1) + ft_strlen(s2)+1);
-	if (!join_spot)
+	else if (!s1)
+		return (ft_strdup(s2));
+	else if (!s2)
+		return (ft_strdup(s1));
+	size = ft_strlen(s1) + ft_strlen(s2);
+	newstr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!newstr)
 		return (NULL);
 	ft_memcpy(join_spot, s1, ft_strlen(s1));
 	len = ft_strlen(s2) + 1;
