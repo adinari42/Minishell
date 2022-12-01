80fb467bd7d86bd21d5b8a6445a22d37e0166fc8
Stephanie Lakner <stephanie.lakner@rub.de>
2022-12-01 18:00:25 +0100

    zwischencommit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 15:26:14 by adinari           #+#    #+#             */
-/*   Updated: 2022/12/01 16:48:11 by adinari          ###   ########.fr       */
+/*   Updated: 2022/12/01 17:59:11 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -269,23 +269,18 @@ void main_loop(int stdin_restore, t_pipe	data)
 	t_token	**list;
 	int		i;
 	char	**envp_c;
-	
+
 	inpt = readline("Minishell$ ");
 	if (!inpt)
 		free_and_exit(SIGINT);
 	inpt_split = ft_split(inpt, '|');
+
 	if (inpt && inpt[0])
 	{
 		i = 0;
 		while (inpt_split[i])
 			i++;
 		add_history(inpt);
-		list = read_tokens(inpt);
-		list = merge_quoted_strings(list);
-		print_list(*list);
-		// printf("After removing spaces: \n");
-		// list = remove_spaces(list);
-		// print_list(*list);
 
 		// char *args[2];
 		// args[0] = "/bin/cat";
@@ -299,20 +294,19 @@ void main_loop(int stdin_restore, t_pipe	data)
 
 					// printf("%s\n", inpt);
 			
-		printf("pos = %d\n", i);
+	// 	printf("pos = %d\n", i);
 		data.cmd_pos = i;
 		i = 0;
-		while(inpt_split[i])
+		while (inpt_split[i])
 		{
 			list = read_tokens(inpt_split[i]);
 			list = merge_quoted_strings(list);
 			list = remove_empty(list); // deletes empty nodes
 			envp_c = env_list_to_char_arr(g_env);
-			// for (int i = 0; envp[i]; i++) //  && ft_strncmp(envp[i], "", 1)
-			// 	printf("envp: %s\n", envp[i]);
+	// 		// for (int i = 0; envp[i]; i++) //  && ft_strncmp(envp[i], "", 1)
+	// 		// 	printf("envp: %s\n", envp[i]);
 			check_value(*list, envp_c);
 			init_path(*list, get_cmd(*list, &data), &data.parse);
-			/***********************************************/
 			data.pid = fork();
 			dup2(stdin_restore, 1);
 			if (data.pid == -1)
@@ -327,13 +321,12 @@ void main_loop(int stdin_restore, t_pipe	data)
 				parent(&data);
 				waitpid(0, &data.pid, 0);
 			}
-			/***********************************************/
 			free_char_arr(envp_c);
 			free_token_list(list);
 			free_parse(&data.parse);
 			i++;
 		}
-			// close(stdin_restore);
+	// 		// close(stdin_restore);
 	}
 	free(inpt);
 	free_char_arr(inpt_split);
@@ -344,8 +337,9 @@ int	main(int argc, char **argv, char **envp)
 {
 	// t_parse	parse;
 	int		stdin_restore;
-	int		stdout_restore;
-	int	err;
+	//int		stdout_restore;
+	t_pipe	data;
+	
 
 	if (argc != 1)
 		return (1);
@@ -357,58 +351,12 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		dup2(stdin_restore, 0);
-		// dup2(stdout_restore, 1);
-		// write(2, "i get here", 10);
-		// close(stdin_restore);
-		// close(stdout_restore);
-		inpt = readline("Minishell$ ");
-		add_history(inpt);
-		inpt_split = ft_split(inpt, '|');
-		free(inpt);
-		if (inpt && inpt[0])
-		{
-			i = 0;
-			while (inpt_split[i])
-				i++;
-			data.cmd_pos = i;
-			i = 0;
-			while(inpt_split[i])
-			{
-				pipe(data.fd);
-				list = read_tokens(inpt_split[i]);
-				list = merge_quoted_strings(list);
-				check_value(*list, envp);
-				data.pid = fork();
-				init_path(*list, get_cmd(*list, &data), &data.parse);
-				/***********************************************/
-				// dup2(stdin_restore, 0);
-				dup2(stdout_restore, 1);
-				if (data.pid == -1)
-					fd_err(4);
-				if (data.pid == 0)
-				{
-					child(&data, i + 1);
-					exec_cmd(&data, envp);
-				}
-				else
-				{
-					parent(&data);	
-					waitpid(data.pid, &err, 0);
-				}
-				/***********************************************/
-				free_token_list(list);
-				free_parse(&data.parse);
-				i++;
-			}
-			//for each pipe
-					//waitpid(0, &data.pid, 0);
-		}
-		free_2d(&inpt_split);
+		dup2(stdout_restore, 1);
+		main_loop(stdin_restore, data);
+		close(stdin_restore);
 		unlink("tmp");
-		// exit(1);
-		// system("leaks minishell");
 	}
 	free_and_close(&data);
-	free_2d(&data.parse.split_envp);
+	free_char_arr(data.parse.split_envp);
 	return (argc);
 }
