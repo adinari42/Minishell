806f4038b4fa1bfc2be7c4f19308130fd43b4974
Stephanie Lakner <stephanie.lakner@rub.de>
2022-12-08 18:58:28 +0100

    adding pipes.c

--- pipes.c
+++ pipes.c
@@ -6,7 +6,7 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 21:38:34 by slakner           #+#    #+#             */
-/*   Updated: 2022/12/13 22:01:55 by slakner          ###   ########.fr       */
+/*   Updated: 2022/12/08 18:05:10 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,39 +51,37 @@ t_token	*cut_at_pipe(t_token *list, t_token **pipes, int i)
 	}
 	if (end->prev)
 		end->prev->next = NULL;
-	free_token(end);
+	free(end);
 	return (next);
 }
 
 // we might have to make a copy of the original list with memcopy since it gets changed here
 // assign i+1 and mark end of array somehow?
-t_token	**list_to_pipes(t_token **list)
+t_token	**list_to_pipes(t_token *list)
 {
 	t_token	**pipes;
 	int		i;
 	int		numpipes;
-	t_token	*elem;
 
-	numpipes = count_tokens(*list, PIPE);
+	numpipes = count_tokens(list, PIPE);
 	pipes = malloc(sizeof(t_token *) * (numpipes + 1));
 	i = 0;
-	elem = *list;
-	if (!list || !elem)
+	if (!list)
 		return (NULL);
-	if (elem->type == PIPE)
+	if (list->type == PIPE)
 		prnt_err("", "", "syntax error near unexpected token `|'");
-	pipes[0] = elem;
-	while (elem->next)
+	pipes[0] = list;
+	while (list->next)
 	{
-		if (elem->type == PIPE)
+		if (list->type == PIPE)
 		{
-			elem = cut_at_pipe(elem, pipes, i);
+			list = cut_at_pipe(list, pipes, i);
 			i++;
 		}
 		else
-			elem = elem->next;
+			list = list->next;
 	}
-	pipes[numpipes - 1] = tlist_start(elem);
+	pipes[numpipes - 1] = tlist_start(list);
 	pipes[numpipes] = NULL;
 	return (pipes);
 }
