df317ab238d54c0b9b05f85e233be64de7e49098
Unknown <stephanie.lakner@rub.de>
2022-12-24 18:52:13 +0100

    Removed unused function

--- includes/parse.h
+++ includes/parse.h
@@ -6,7 +6,7 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 20:24:05 by slakner           #+#    #+#             */
-/*   Updated: 2022/12/19 23:57:45 by slakner          ###   ########.fr       */
+/*   Updated: 2022/12/24 18:51:38 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,4 @@ int		parse(t_token **list, t_pipe *data);
 int		is_empty_inpt(char *inpt);
 int		check_pipes(t_token *tkn);
 t_token	*remove_spaces(t_token *list);
-t_token	**remove_empty(t_token **list);
 #endif
